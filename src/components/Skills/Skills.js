e19cd5ce9d26a4e5112359533f623bf867590377
Your Name <you@example.com>
2023-03-14 22:45:26 +0400

    update skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -11,16 +11,13 @@ const Skills = () => {
                 <div className="row">
                     <div className="col-lg-6 offset-lg-3">
                         <div className="skills-holder">
-                            <ProgressBar skill={'C++'} progressPercent={'80%'} progressWidth={'80%'} />
-                            <ProgressBar skill={'SQL'} progressPercent={'60%'} progressWidth={'60%'} />
-                            <ProgressBar skill={'PHP'} progressPercent={'60%'} progressWidth={'60%'} />
+                            <ProgressBar skill={'C++'} progressPercent={'60%'} progressWidth={'60%'} />
                             <ProgressBar skill={'JavaScript'} progressPercent={'70%'} progressWidth={'70%'} />
                             <ProgressBar skill={'HTML5, CSS3'} progressPercent={'90%'} progressWidth={'90%'} />
                             <ProgressBar skill={'Adobe Photoshop'} progressPercent={'90%'} progressWidth={'90%'} />
                             <ProgressBar skill={'Adobe Illustrator'} progressPercent={'90%'} progressWidth={'90%'} />
                             <ProgressBar skill={'Adobe Premiere'} progressPercent={'70%'} progressWidth={'70%'} />
                             <ProgressBar skill={'UI,UX design'} progressPercent={'70%'} progressWidth={'70%'} />
-                            <ProgressBar skill={'AutoCad'} progressPercent={'40%'} progressWidth={'40%'} />
                             <ProgressBar skill={'Microsoft Word'} progressPercent={'90%'} progressWidth={'90%'} />
                             <ProgressBar skill={'Microsoft PowerPoint'} progressPercent={'90%'} progressWidth={'90%'} />
                             <ProgressBar skill={'Microsoft Excel'} progressPercent={'60%'} progressWidth={'60%'} />
